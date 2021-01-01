849822d52b98aaff959ab1c4ad804f37234c7a71
AdamTMalek <malekadas@gmail.com>
2021-01-01 17:28:41 +0000

    Add logout button to the navigation

--- atmalblog/urls.py
+++ atmalblog/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('logout', views.logout_view, name='logout')
+    path('new-post', views.new_post, name='new_post'),
+    path('logout', views.logout_view, name='logout'),
 ]
--- atmalblog/views.py
+++ atmalblog/views.py
@@ -1,10 +1,16 @@
 from django.contrib.auth import logout
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+@staff_member_required
+def new_post(request):
+    return render(request, 'new_post.html')
 
 
 @require_http_methods(['POST'])
