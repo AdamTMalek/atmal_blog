918065a4f7107905a36dced552a2581030a8e74e
AdamTMalek <malekadas@gmail.com>
2021-01-01 17:29:11 +0000

    Add markdownx dependency

--- atmal_blog/urls.py
+++ atmal_blog/urls.py
@@ -1,8 +1,9 @@
 """atmal_blog URL Configuration"""
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('atmalblog.urls'))
+    path('', include('atmalblog.urls')),
+    re_path(r'^markdownx/', include('markdownx.urls')),
 ]
