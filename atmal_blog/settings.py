064c60ad6deeda4318717e85d36ad1b49ae0082b
AdamTMalek <malekadas@gmail.com>
2021-01-01 18:38:50 +0000

    Add pygments dependency for syntax highlighting

--- atmal_blog/settings.py
+++ atmal_blog/settings.py
@@ -114,6 +114,9 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Markdownx
+MARKDOWNX_MARKDOWN_EXTENSIONS = ['markdown.extensions.extra', 'markdown.extensions.codehilite']
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
