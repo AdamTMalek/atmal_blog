810f2b89099ae40b7528bcea7eccacf216eeb9f3
AdamTMalek <malekadas@gmail.com>
2021-01-01 16:04:05 +0000

    Add logout button to the navigation

--- atmalblog/urls.py
+++ atmalblog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('logout', views.logout_view, name='logout')
 ]
