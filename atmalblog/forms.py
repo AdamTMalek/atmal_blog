0b27ef42717ded8499674e7d4c24c130904e3cc0
AdamTMalek <malekadas@gmail.com>
2021-01-16 18:42:13 +0000

    Add series translation formset

--- atmalblog/forms.py
+++ atmalblog/forms.py
@@ -1,4 +1,4 @@
-from django.forms import Form, CharField
+from django.forms import Form, formset_factory, CharField
 
 from atmalblog.models import *
 
@@ -6,3 +6,6 @@ from atmalblog.models import *
 class SeriesTranslationForm(Form):
     name = CharField(max_length=SeriesTranslations._meta.get_field('name').max_length)
     language = CharField(max_length=SeriesTranslations._meta.get_field('language').max_length)
+
+
+SeriesTranslationFormSet = formset_factory(SeriesTranslationForm, extra=0)
