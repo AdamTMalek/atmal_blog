a13ea5ecc66fa6e58846d1c4a45dd7e0d30d1559
AdamTMalek <malekadas@gmail.com>
2021-01-16 18:42:48 +0000

    Write views for adding new post (just adding new series)

--- atmalblog/views.py
+++ atmalblog/views.py
@@ -1,7 +1,15 @@
-from django.contrib.auth import logout
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth import logout
+from django.core import serializers
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
+
+from .forms import *
+
+
+def _get_initial_series_translation_form_set_data():
+    return [{'name': '', 'language': lang} for lang in Language.objects.all()]
 
 
 def index(request):
@@ -10,7 +18,32 @@ def index(request):
 
 @staff_member_required
 def new_post(request):
-    return render(request, 'new_post.html')
+    if request.method == 'POST':
+        pass
+
+    context = {
+        'series_translation_form_set': SeriesTranslationFormSet(initial=_get_initial_series_translation_form_set_data())
+    }
+
+    return render(request, 'new_post.html', context=context)
+
+
+@staff_member_required
+@require_http_methods(['POST'])
+def new_series(request):
+    form_set = SeriesTranslationFormSet(request.POST, initial=_get_initial_series_translation_form_set_data())
+
+    if form_set.is_valid():
+        series = Series.objects.create()
+        for form in form_set:
+            form_name = form.cleaned_data['name']
+            form_lang = Language.objects.get(native_name=form.cleaned_data['language'])
+            SeriesTranslations.objects.create(series=series, name=form_name, language=form_lang)
+
+        serialized_series = serializers.serialize('json', [series])
+        return JsonResponse(serialized_series, status=200, safe=False)
+    else:
+        return JsonResponse({'errors': form_set.errors}, status=400)
 
 
 @require_http_methods(['POST'])
