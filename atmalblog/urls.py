1c2b3c672d2aa241a934799af342513ae00cafae
AdamTMalek <malekadas@gmail.com>
2021-01-16 18:43:10 +0000

    Add add_series view to the urls

--- atmalblog/urls.py
+++ atmalblog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('new-post', views.new_post, name='new_post'),
+    path('new-post/new-series', views.new_series, name='new_series'),
     path('logout', views.logout_view, name='logout'),
 ]
