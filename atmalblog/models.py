6e7092c6c7cb9b8a46bc9dea690b1a6097fd58f8
AdamTMalek <malekadas@gmail.com>
2021-01-17 16:42:59 +0000

    Remove unique constraints on names for series and category

--- atmalblog/models.py
+++ atmalblog/models.py
@@ -25,7 +25,7 @@ class Category(models.Model):
 
 class CategoryTranslation(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    name = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20)
     language = models.ForeignKey(Language, on_delete=models.PROTECT)
 
     class Meta:
@@ -51,7 +51,7 @@ class SeriesTranslations(models.Model):
     Holds the translated series title
     """
     series = models.ForeignKey(Series, on_delete=models.PROTECT)
-    name = models.CharField(max_length=25, unique=True)
+    name = models.CharField(max_length=25)
     language = models.ForeignKey(Language, on_delete=models.PROTECT)
 
     class Meta:
