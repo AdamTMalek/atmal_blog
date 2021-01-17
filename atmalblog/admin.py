427e6a128d033f25e4efb59aec1ab9c2c55294d5
AdamTMalek <malekadas@gmail.com>
2021-01-17 21:24:21 +0000

    Create custom admin entry for post translation

--- atmalblog/admin.py
+++ atmalblog/admin.py
@@ -1,11 +1,15 @@
-from django.contrib.admin.sites import AlreadyRegistered
-from django.contrib import admin
 from django.apps import apps
-from markdownx.admin import MarkdownxModelAdmin
+from django.contrib.admin.sites import AlreadyRegistered
+from markdownx.admin import *
+
 from .models import *
 
 
-admin.site.register(PostTranslations, MarkdownxModelAdmin)
+@admin.register(PostTranslations)
+class PostTranslationAdmin(admin.ModelAdmin):
+    fields = ('post', 'title', 'short_description', 'language')
+    list_display = ('post', 'title', 'short_description', 'published_date')
+
 
 models = apps.get_app_config('atmalblog').get_models()
 for model in models:
