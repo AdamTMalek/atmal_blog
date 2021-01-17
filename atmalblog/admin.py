a187c77de80dbe3be451a01dd27132835a6297b3
AdamTMalek <malekadas@gmail.com>
2021-01-17 17:45:16 +0000

    Register every model belonging to the app in admin

--- atmalblog/admin.py
+++ atmalblog/admin.py
@@ -1,7 +1,15 @@
+from django.contrib.admin.sites import AlreadyRegistered
 from django.contrib import admin
+from django.apps import apps
 from markdownx.admin import MarkdownxModelAdmin
 from .models import *
 
 
 admin.site.register(PostTranslations, MarkdownxModelAdmin)
-admin.site.register(Language)
+
+models = apps.get_app_config('atmalblog').get_models()
+for model in models:
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered:
+        pass
