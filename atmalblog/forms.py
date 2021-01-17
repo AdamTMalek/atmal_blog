617d190baa8bc9ab29dca6e3bd71ac953a3a4179
AdamTMalek <malekadas@gmail.com>
2021-01-17 11:54:31 +0000

    Add meta fields to post (required, max length etc.)

--- atmalblog/forms.py
+++ atmalblog/forms.py
@@ -1,6 +1,26 @@
-from django.forms import Form, formset_factory, CharField
+from django.forms import *
+from markdownx.fields import MarkdownxFormField
 
 from atmalblog.models import *
+
+
+class OriginalPostForm(ModelForm):
+    class Meta:
+        model = PostTranslations
+        fields = ['title', 'short_description', 'language', 'content']
+
+
+class PostForm(Form):
+    categories = ModelMultipleChoiceField(queryset=Category.objects.all(),
+                                          required=(not Post._meta.get_field('categories').blank))
+    series = ModelChoiceField(queryset=Series.objects.all(),
+                              empty_label="(No series)",
+                              required=(not Post._meta.get_field('series').blank))
+    thumbnail = ImageField(required=(not Post._meta.get_field('thumbnail').blank))
+    title = CharField(max_length=PostTranslations._meta.get_field('title').max_length)
+    short_description = CharField(max_length=PostTranslations._meta.get_field('short_description').max_length)
+    content = MarkdownxFormField()
+    language = ModelChoiceField(queryset=Language.objects.all(), empty_label="(Choose a language)")
 
 
 class SeriesTranslationForm(Form):
