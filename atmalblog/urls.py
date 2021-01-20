b5b1b57a885de5999e00a1cf3b3a2efeb960f1b5
AdamTMalek <malekadas@gmail.com>
2021-01-20 16:08:55 +0000

    Add media to URLs

--- atmalblog/urls.py
+++ atmalblog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -8,4 +10,5 @@ urlpatterns = [
     path('new-post/new-series', views.new_series, name='new_series'),
     path('new-post/new-category', views.new_category, name='new_category'),
     path('logout', views.logout_view, name='logout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
