a337ef5eaffe859c141ac4b144c4cb6ebeaa2b04
AdamTMalek <malekadas@gmail.com>
2021-01-21 20:34:24 +0000

    Fix 404 markdownx issues by including markdownx.urls before app-specific

--- atmal_blog/urls.py
+++ atmal_blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('markdownx/', include('markdownx.urls')),
     path('', include('atmalblog.urls')),
-    re_path(r'^markdownx/', include('markdownx.urls')),
 ]
