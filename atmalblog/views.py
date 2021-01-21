f18e76d195a5c05cf334569e745eef42f50205bd
AdamTMalek <malekadas@gmail.com>
2021-01-21 20:41:08 +0000

    Display posts on home page

--- atmalblog/views.py
+++ atmalblog/views.py
@@ -1,6 +1,5 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth import logout
-from django.core import serializers
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
@@ -13,16 +12,43 @@ def _get_initial_form_set_data():
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'posts': PostTranslations.objects.all()
+    }
+    return render(request, 'index.html', context=context)
 
 
 @staff_member_required
 def new_post(request):
     if request.method == 'POST':
-        pass
+        # Series and category forms are handled by other view functions.
+        # Here, we only deal with the post form.
+
+        post_form = PostForm(request.POST, request.FILES)
+        if post_form.is_valid():
+            # Create post
+            categories = post_form.cleaned_data['categories']
+            series = post_form.cleaned_data['series']
+            thumbnail = post_form.cleaned_data['thumbnail']
+            post = Post.objects.create(series=series, thumbnail=thumbnail)
+            post.categories.set(categories)
+
+            # Create translation
+            title = post_form.cleaned_data['title']
+            short_desc = post_form.cleaned_data['short_description']
+            content = post_form.cleaned_data['content']
+            language = Language.objects.get(native_name=post_form.cleaned_data['language'])
+            PostTranslations.objects.create(post=post,
+                                            title=title,
+                                            short_description=short_desc,
+                                            content=content,
+                                            language=language)
+            return redirect('index')
+    else:
+        post_form = PostForm()
 
     context = {
-        'post_form': PostForm(),
+        'post_form': post_form,
         'series_translation_form_set': SeriesTranslationFormSet(initial=_get_initial_form_set_data()),
         'category_translation_form_set': CategoryTranslationFormSet(initial=_get_initial_form_set_data()),
     }
