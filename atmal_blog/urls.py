de23660b0a0cb764b804f3cf17c5cbf190e244d5
AdamTMalek <malekadas@gmail.com>
2021-01-26 14:56:44 +0000

    Remove unused import

--- atmal_blog/urls.py
+++ atmal_blog/urls.py
@@ -1,6 +1,6 @@
 """atmal_blog URL Configuration"""
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
