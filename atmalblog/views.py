62bf45b9b94d3764434964180ceea8dca2ffe551
AdamTMalek <malekadas@gmail.com>
2021-01-26 16:39:46 +0000

    Order posts on index by published date

--- atmalblog/views.py
+++ atmalblog/views.py
@@ -17,7 +17,7 @@ def _get_initial_form_set_data():
 
 def index(request):
     context = {
-        'posts': PostTranslations.objects.all()
+        'posts': PostTranslations.objects.order_by('-published_date')
     }
     return render(request, 'index.html', context=context)
 
